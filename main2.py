9f7873488616ce651c1844ec266e0719b4dede2d
Rojas-Hernandez <irojas-hern@cps.k12.il.us>
2025-04-04 12:02:31 -0500

    first commit

---
+++ main2.py
@@ -0,0 +1,29 @@
+# what are varbles?
+
+# there are different types of varbles
+# string are text
+name = "John" #quotation marks
+name = "Lucy"
+# integers are whole numbers
+num1 = "10" #quotation marks
+num2 = 20
+print(int(num1 + num2)) # when you have
+# a plus sign between two varbiables its called
+# concatenation
+# type casting when you convert a
+# varbiables from one type to another
+# print(name + "and"+ name2 + "have"+ num1 + str(num2))
+# f strings allow us to insert variables into strings
+# u f before the string
+print(f"{name} and {num2} have {num1} apples")
+# floats are decimal numbers
+dollars = 10.99
+# it can be positive or negative
+print(f"{name} has {dollars} dollars")
+# booleans are true or false
+is_student= True
+# it can be true or false
+print(f"{name} is a student: {is_student}")
+# lists are collections of value
+# dict
+
---
+++ masin.py
@@ -0,0 +1,11 @@
+# print("I like pizza")
+# print("It's really good!")
+
+first_name= "Bro"
+food= "pizza"
+email= "Bro123@fake.com"
+# f-string allows you 
+
+print(f"Hello {first_name}")
+print(f"You like {food}")
+print(f"Your email is:{email}")
