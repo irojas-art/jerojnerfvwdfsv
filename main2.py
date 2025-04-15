9402f74b339d333a2260f840f5585200824e11e4
Rojas-Hernandez <irojas-hern@cps.k12.il.us>
2025-04-15 11:47:48 -0500

    conitionals

--- main2.py
+++ main2.py
@@ -1,29 +1,63 @@
-# what are varbles?
+# # what are varbles?
 
-# there are different types of varbles
-# string are text
-name = "John" #quotation marks
-name = "Lucy"
-# integers are whole numbers
-num1 = "10" #quotation marks
-num2 = 20
-print(int(num1 + num2)) # when you have
-# a plus sign between two varbiables its called
-# concatenation
-# type casting when you convert a
-# varbiables from one type to another
-# print(name + "and"+ name2 + "have"+ num1 + str(num2))
-# f strings allow us to insert variables into strings
-# u f before the string
-print(f"{name} and {num2} have {num1} apples")
-# floats are decimal numbers
-dollars = 10.99
-# it can be positive or negative
-print(f"{name} has {dollars} dollars")
-# booleans are true or false
-is_student= True
-# it can be true or false
-print(f"{name} is a student: {is_student}")
-# lists are collections of value
-# dict
+# # there are different types of varbles
+# # string are text
+# name = "John" #quotation marks
+# name = "Lucy"
+# # integers are whole numbers
+# num1 = "10" #quotation marks
+# num2 = 20
+# print(int(num1 + num2)) # when you have
+# # a plus sign between two varbiables its called
+# # concatenation
+# # type casting when you convert a
+# # varbiables from one type to another
+# # print(name + "and"+ name2 + "have"+ num1 + str(num2))
+# # f strings allow us to insert variables into strings
+# # u f before the string
+# print(f"{name} and {num2} have {num1} apples")
+# # floats are decimal numbers
+# dollars = 10.99
+# # it can be positive or negative
+# print(f"{name} has {dollars} dollars")
+# # booleans are true or false
+# is_student= True
+# # it can be true or false
+# print(f"{name} is a student: {is_student}")
+# # lists are collections of value
+# # dict
+
+
+# if conditionals
+# if statements are used to check if a conditon is turn or false
+# if condition is true, do something
+# if condition is true, do something else
+
+# if conditionals statements alawys start with if
+# and depend on a boolean value (ture or false)
+# example
+classStared = True # boolean variable
+if classStared:
+    
+
+
+# logial and comparison opprators
+# == equal to
+# != not equal to
+# > greater than
+# < less than
+# >= greater than or equal to
+# <= less than or equal to
+# and logial operators
+# or logial operators
+# not logial operators
+# example
+age = 18
+if age >= 18:
+    print("you nare an adult")
+elif age == 17
+    print("you are amost an adult")
+else:
+    print("you are a minor")
+
 
